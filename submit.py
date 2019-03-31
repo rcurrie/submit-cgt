f514ab8d157ec9c8b580692b86dce01350a9a8c2
Rob Currie <rcurrie@ucsc.edu>
2019-03-31 09:01:01 -0700

    Large update to conform ot the new simpler structure of just files. Much faster submission when there are 1000s of files in a submission. Simple tracking of top level hash in a .cgt file

--- submit.py
+++ submit.py
@@ -2,65 +2,89 @@
 Submit files to an IPFS server conforming to the CGT submission
 specification
 """
+import os
+import json
 import glob
 import argparse
-import json
 import ipfsapi
 
 
-parser = argparse.ArgumentParser(
-    description="Make a CGT submission to an IPFS server")
-parser.add_argument('--id', required=True,
-                    help="Patient CGT ID")
-parser.add_argument('--days', required=True,
-                    help="Days from birth of submission")
-parser.add_argument('--path', required=True,
-                    help="Path to files for submission")
-args = parser.parse_args()
+def get_index(ipfs, domain):
+    """ Get the current index using ./.cgt or create one if not existent """
+    if os.path.exists(".cgt"):
+        print("Loading existing index hash from .cgt")
+        with open(".cgt") as f:
+            index_hash = f.read()
+        index = json.loads(ipfs.cat(index_hash))
+    else:
+        print("No existing index hash, creating empty default")
+        index = {"domain": domain, "submissions": []}
+        index_hash = ipfs.add_str(json.dumps(index, sort_keys=True))
+        with open(".cgt", "w") as f:
+            f.write(index_hash)
+    return index, index_hash
 
-print(args)
 
-ipfs = ipfsapi.Client(host="127.0.0.1", port=5001)
-print("Connected")
+def add_submission(ipfs, submission_hash):
+    """ Add the submission to the index if not already there """
+    with open(".cgt") as f:
+        index_hash = f.read()
+    index = json.loads(ipfs.cat(index_hash))
+    if submission_hash in index["submissions"]:
+        print("Submission already in steward's index")
+    else:
+        index["submissions"] = sorted(
+            index["submissions"] + [submission_hash])
+        print("New steward index", index)
+
+        index_hash = ipfs.add_str(json.dumps(index, sort_keys=True))
+        print("New index hash", index_hash)
+
+        with open(".cgt", "w") as f:
+            f.write(index_hash)
 
 
-# Add all the files to IPFS
-paths = glob.glob("{}/**/*".format(args.path), recursive=True)
-print("Adding {} files".format(len(paths)))
-print(paths)
-files = [ipfs.add(path) for path in paths]
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Make a CGT submission to an IPFS server")
+    parser.add_argument('--id', required=True,
+                        help="Public patient identifier")
+    parser.add_argument('--path', required=True,
+                        help="Path to files for submission")
+    parser.add_argument('--domain', required=False,
+                        help="Domain to initialize index")
+    parser.add_argument('--days', required=False,
+                        help="Days from birth of submission")
+    args = parser.parse_args()
 
-# Add the submission to IPFS
-submission = {"cgt_id": args.id, "days_from_birth": int(args.days)}
-submission["files"] = sorted([{"name": f["Name"], "multihash": f["Hash"]} for f in files],
-                             key=lambda k: k["name"] + k["multihash"])
-print("submission", submission)
-submission_multihash = ipfs.add_str(json.dumps(submission, sort_keys=True))
-print("submission_multihash", submission_multihash)
+    print("Connecting to infura...")
+    ipfs = ipfsapi.connect("https://ipfs.infura.io", 5001)
 
-# Update steward index
-steward_ipns_address = ipfs.id()["ID"]
-print("Steward IPNS Address", steward_ipns_address)
+    print("Getting index...")
+    index, index_hash = get_index(ipfs, args.domain)
 
-steward_index_cid = ipfs.name_resolve(
-    steward_ipns_address, opts={'local': True})["Path"].rsplit('/')[-1]
-print("Steward index cid", steward_index_cid)
+    # Add all the files to IPFS
+    files = []
+    paths = glob.glob("{}/**/*".format(args.path), recursive=True)
+    count = len(paths)
+    for path in paths:
+        if os.path.isdir(path):
+            continue
+        print("Remaining: {} File: {} Size: {}".format(
+            count, os.path.basename(path), os.path.getsize(path)), end="\r")
+        files.append(ipfs.add(path))
+        count -= 1
 
-steward_index = json.loads(ipfs.cat(steward_index_cid))
-print("Steward index", steward_index)
+    print("Added {} files".format(len(files)))
 
-if submission_multihash in steward_index["submissions"]:
-    print("Submission already in steward's index")
-else:
-    steward_index["submissions"] = sorted(steward_index["submissions"] + [submission_multihash])
-    print("new steward_index", steward_index)
+    # Add the submission to IPFS
+    submission = {"publicId": args.id}
+    if args.days:
+        submission["daysFromBirth"] = args.days
+    submission["files"] = sorted([{"name": f["Name"], "multihash": f["Hash"]} for f in files],
+                                 key=lambda k: k["name"] + k["multihash"])
+    print("submission", submission)
+    submission_hash = ipfs.add_str(json.dumps(submission, sort_keys=True))
+    print("submission_multihash", submission_hash)
 
-    steward_index_cid = ipfs.add_str(json.dumps(steward_index, sort_keys=True))
-    print("new steward_index_cid", steward_index_cid)
-
-    print("Publishing...")
-    ipfs.name_publish(ipfs_path="/ipfs/{}".format(steward_index_cid),
-                      resolve=False,
-                      lifetime="24h")
-
-print("Done.")
+    add_submission(ipfs, submission_hash)
